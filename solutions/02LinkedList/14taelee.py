2040bbf5337d801889dfe26ab62bab9761bd40bf
taelee <taelee@student.42seoul.kr>
2020-12-22 16:44:00 +0900

    add 02LinkedList/14taelee.py

---
+++ solutions/02LinkedList/14taelee.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+
+        res = cur = ListNode(0)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                cur = l1
+                l1 = l1.next
+            else:
+                l1
+            curr = curr.next
+            print(curr1.val)
+            print(curr2v)
+            l1 =.next
+            curr2 = curr2.next
