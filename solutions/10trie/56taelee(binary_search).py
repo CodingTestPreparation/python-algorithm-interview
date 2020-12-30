261ee3e33800a460fd763728f1dcb91babf685ea
taelee <taelee@student.42seoul.kr>
2020-12-30 10:51:05 +0900

    add 56taelee(binary_search).py

--- solutions/10trie/56taelee(binary_search).py
+++ solutions/10trie/56taelee(binary_search).py
@@ -5,21 +5,11 @@ def count_by_range(a, left_value, right_value):
     left_index = bisect_left(a, left_value)
     return right_index - left_index
 
-# a = ["ab", "aa", "az", "ad"]
-# a.sort()
-# print(count_by_range(a, "ab", "ac"))
-
 class Trie:
     def __init__(self):
         self.dict = {}
-        """
-        Initialize your data structure here.
-        """
 
     def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
         if not len(word) in self.dict:
             self.dict[len(word)] = []
         self.dict[len(word)].append(word)
@@ -31,9 +21,6 @@ class Trie:
         if count_by_range(self.dict[len(word)], word, word) != 0:
             return True
         return False
-        """
-        Returns if the word is in the trie.
-        """
 
     def startsWith(self, prefix: str) -> bool:
         for key in self.dict:
@@ -46,12 +33,7 @@ class Trie:
                 return True
         return False
 
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
-
-# Your Trie object will be instantiated and called as such:
-# word = "apple"
+# Test code
 obj = Trie()
 obj.insert("app")
 obj.insert("apple")
@@ -60,11 +42,4 @@ obj.insert("add")
 obj.insert("jam")
 obj.insert("rental")
 obj.search("apps")
-print(obj.dict[3])
 print(obj.search("app"))
-# print(obj.hi)
-# obj.insert(word)
-# param_2 = obj.search(word)
-# print(obj.search("appl"))
-# print(obj.startsWith("appled"))
-# param_3 = obj.startsWith("app")
